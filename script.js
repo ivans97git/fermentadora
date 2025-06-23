daca64a2bc3db075a696f16fef2c4108c6268193
ivans97git <ivansegoviamiranda@gmail.com>
2025-06-23 18:07:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Configuración MQTT (HiveMQ Cloud)
 const broker = "90883d7a8ff64950af6e002e4bd77ee3.s1.eu.hivemq.cloud"; // Cambia por tu broker
 const port = 8884; // Puerto para WebSocket (SSL)
-const topic = "casa/sensores";
+const topic = "casa/dht11";
 
 // Elementos del DOM
 const temperatureElement = document.getElementById("temperature");
@@ -24,7 +24,6 @@ client.onMessageArrived = (message) => {
         const data = JSON.parse(message.payloadString);
         temperatureElement.textContent = data.temperatura;
         humidityElement.textContent = data.humedad;
-        porcetajeNivelElement.textContent = data.porcentajenivel;
         console.log("Datos actualizados:", data);
     } catch (error) {
         console.error("Error al procesar mensaje:", error);
